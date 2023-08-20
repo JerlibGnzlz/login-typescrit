4c992f31769f6c126daa9953d88df1d9761a7d60
jerlibgnzlz <jerlibgv@gmail.com>
2023-08-20 03:36:06 -0300

    clean codigo

--- Interfaces/user.Interfaces.ts
+++
@@ -1,9 +0,0 @@
-// export interface IUser {
-//     role: string
-//     nombre: string,
-//     email: string,
-//     password: string,
-//     confirmarPassword: string,
-//     passwordHashado(password: string): Promise<string>
-//     passwordCorrecto(password: string): Promise<boolean>
-// }
